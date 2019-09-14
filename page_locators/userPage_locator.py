d3e314e9c694d2a6cc6e48ae0c7a9cae443a13b6
“2020668” <keen2020@outlook.com>
2019-09-14 09:33:31 +0800

    version 20190914

--- page_locators/userPage_locator.py
+++ page_locators/userPage_locator.py
@@ -15,4 +15,4 @@ from selenium.webdriver.common.by import By
 class UserPageLocator:
 
     # 可用余额
-    user_leftMoney = (By.XPATH,'//li[@class="color_sub"]')
+    user_leftMoney = (By.XPATH, '//li[@class="color_sub"]')
